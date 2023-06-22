1edb36f9cf347dded096672d35d72447c4088188
Filippo Bristot <filippo.bristot@hotmail.it>
2023-06-22 16:53:03 +0200

    feat: add random word from list

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 import { wordleLa } from './wordleLa.js'
 import { wordleTa } from './wordleTa.js'
 const root = document.getElementById('root');
-const WORD = 'panic';
+const WORD = selectRandomWord()
 const ROWS = 6;
 const COLS = 5;
 let CURRENT_ROW = 0
 let GAME_STATE = 'PLAY'
 const wordTracker = Array.from({ length: ROWS }, () => Array.from({ length: COLS }, () => ''))
-console.log(wordTracker)
+// console.log(wordTracker)
 
 let headerContainer = null
 let wordleContainer = null
@@ -25,6 +25,15 @@ const wordIs = {
 const backSpaceSVG = `<svg style="width:1.5rem" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 <path stroke-linecap="round" stroke-linejoin="round" d="M12 9.75L14.25 12m0 0l2.25 2.25M14.25 12l2.25-2.25M14.25 12L12 14.25m-2.58 4.92l-6.375-6.375a1.125 1.125 0 010-1.59L9.42 4.83c.211-.211.498-.33.796-.33H19.5a2.25 2.25 0 012.25 2.25v10.5a2.25 2.25 0 01-2.25 2.25h-9.284c-.298 0-.585-.119-.796-.33z" />
 </svg>`
+
+
+/**
+ * @return  A random word.
+ */
+function selectRandomWord() {
+  let words = Array.from(wordleLa);
+  return words[Math.floor(Math.random() * words.length)];
+}
 /* ============================================ */
 /* ··········································· § ROOT STYLES ··· */
 /* ======================================== */
